adb2a2de2431ff356ed03bfec27ba7a8c6a97c5b
jeandokill <157924037+jeandokill@users.noreply.github.com>
2024-02-22 13:27:39 +0200

    Update find.php

--- TheEvent/find.php
+++ TheEvent/find.php
@@ -4,22 +4,17 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>MEDCANCER Initiative Rwanda </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/favicon.png" rel="apple-touch-icon">
-
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-
-
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -27,169 +22,134 @@
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
   <link href="assets/css/find.css" rel="stylesheet">
 </head>
 
 <body>
-
   <!-- ======= Header ======= -->
-  <header id="header" class="d-flex align-items-center ">
+ <!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center ">
     <div class="container-fluid container-xxl d-flex align-items-center">
+        <div id="logo" class="me-auto">
+            <a href="index.php" class="scrollto"><img src="assets/img/FAVICON.jpg" alt="" title=""></a>
+        </div>
 
-      <div id="logo" class="me-auto">
-        <a href="index.html" class="scrollto"><img src="assets/img/FAVICON.jpg" alt="" title=""></a>
-      </div>
+        <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="awareness.php">Awareness</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="About Us.php">About Us</a></li>
+                <li><a href="Blog.php">Blog</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><button onclick="location.href='/EVENT/login/login.php'" class="login-button"style="background-color: darkslategray; border-radius: 10px; font-weight: bold; margin-left: 20px; color: lavenderblush;">Login</button></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
 
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a  href="index.php">Home</a></li>
-          <li><a  href="awareness.php">Awareness</a></li>
-          <li><a  href="services.php">Services</a></li>
-          <li><a  href="About Us.php">About Us</a></li>
-          <li><a  href="Blog.php">Blog</a></li>
-          <li><a  href="contact.php">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-      <a href="donate.php" id="donate-button">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Donate
-    </a>
+        <div class="social-links">
+            <a href="https://twitter.com/MedcancerRwa?t=cCk6RnNfQDc8xrXO5Uqm4A&s=09" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=61552968870278&mibextid=LQQJ4d" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/medcancer2023_rwa?igsh=NTc4MTIwNjQ2YQ==" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="https://rw.linkedin.com/in/medcancer-initiative-rwanda-5a600b29a?utm_source=share&utm_medium=member_mweb&utm_campaign=share_via&utm_content=profile" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div>
+
+        <a href="donate.php" id="donate-button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Donate
+        </a>
+
+    </div> 
     
-    </div>
-    
-  </header>
-
-  <div class="header-social-links" style="margin-left: 500px; margin-bottom: 20px; margin-top: 90px;">
-              <a href="https://twitter.com/MedcancerRwa?t=cCk6RnNfQDc8xrXO5Uqm4A&s=09" class="twitter"><i class="bi bi-twitter"></i></a>
-              <a href="https://www.facebook.com/profile.php?id=61552968870278&mibextid=LQQJ4d" class="facebook"><i class="bi bi-facebook"></i></a>
-              <a href="https://www.instagram.com/medcancer2023_rwa?igsh=NTc4MTIwNjQ2YQ==" class="instagram"><i class="bi bi-instagram"></i></a>
-              <a href="https://rw.linkedin.com/in/medcancer-initiative-rwanda-5a600b29a?utm_source=share&utm_medium=member_mweb&utm_campaign=share_via&utm_content=profile" class="linkedin"><i class="bi bi-linkedin"></i></a>
-  </div>
-  <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-  <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-  
+</header><!-- End Header -->
   <main id="main" data-aos="fade" data-aos-delay="1500">
-  
-      <!-- ======= End Page Header ======= -->
-      <div class="page-header d-flex align-items-center">
-        <div class="container position-relative">
-          <div class="row d-flex justify-content-center">
-            <div class="col-lg-6 text-center">
-              <h2 style="margin-top: 50px;">Contact</h2>
-              <p>We work 24/7 and anytime you need help from us, we are available everytime</p>
-            </div>
+    <!-- ======= End Page Header ======= -->
+    <div class="page-header d-flex align-items-center" style="margin-top: 100px;">
+      <div class="container position-relative">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-6 text-center">
+            <h2 style="margin-top: 50px;">Contact</h2>
+            <p>We work 24/7 and anytime you need help from us, we are available everytime</p>
           </div>
         </div>
-      </div><!-- End Page Header -->
-  
-      <!-- ======= Contact Section ======= -->
-      <section id="contact" class="contact">
-        <div class="container">
+      </div>
+    </div><!-- End Page Header -->
 
-            <div class="row gy-4 justify-content-center">
-        
-              <div class="col-lg-3">
-                <div class="info-item d-flex">
-                  <i class="bi bi-geo-alt flex-shrink-0"></i>
-                  <div>
-                    <h4>Location:</h4>
-                    <p>HUYE, RWANDA</p>
-                  </div>
-                </div>
-              </div><!-- End Info Item -->
-        
-              <div class="col-lg-3">
-                <div class="info-item d-flex">
-                  <i class="bi bi-envelope flex-shrink-0"></i>
-                  <div>
-                    <h4>Email:</h4>
-                    <p>medcancerinitiative<br>rwanda030@gmail.com</p>
-                  </div>
-                </div>
-              </div><!-- End Info Item -->
-        
-              <div class="col-lg-3">
-                <div class="info-item d-flex">
-                  <i class="bi bi-phone flex-shrink-0"></i>
-                  <div>
-                    <h4>Call:</h4>
-                    <p>+250781182207</p>
-                  </div>
-                </div>
-              </div><!-- End Info Item -->
-        
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-4">
+            <div class="info-item d-flex">
+              <i class="bi bi-geo-alt flex-shrink-0"></i>
+              <div>
+                <h4>Location:</h4>
+                <p>HUYE, RWANDA</p>
+              </div>
             </div>
-        
-            <div class="row justify-content-center mt-4">
-        
-              <div class="col-lg-9">
-                <form action="contacts.php" method="post" role="form" class="php-email-form">
-                  <div class="row">
-                    <div class="col-md-6 form-group">
-                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                    </div>
-                    <div class="col-md-6 form-group mt-3 mt-md-0">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                    </div>
-                  </div>
-                  <div class="form-group mt-3">
-                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-                  </div>
-                  <div class="form-group mt-3">
-                    <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-                  </div>
-                  <div class="my-3">
-                    <div class="loading">Loading</div>
-                    <div class="error-message"></div>
-                    <div class="sent-message">Your message has been sent. Thank you!</div>
-                  </div>
-                  <div class="text-center"><button type="submit">Send Message</button></div>
-                </form>
-              </div><!-- End Contact Form -->
-        
+          </div><!-- End Info Item -->
+          <div class="col-lg-4">
+            <div class="info-item d-flex">
+              <i class="bi bi-envelope flex-shrink-0"></i>
+              <div>
+                <h4>Email:</h4>
+                <p>medcancerinitiative<br>rwanda030@gmail.com</p>
+              </div>
             </div>
-        
-          </div>
-      </section><!-- End Contact Section -->
+          </div><!-- End Info Item -->
+          <div class="col-lg-4">
+            <div class="info-item d-flex">
+              <i class="bi bi-phone flex-shrink-0"></i>
+              <div>
+                <h4>Call:</h4>
+                <p>+250781182207</p>
+              </div>
+            </div>
+          </div><!-- End Info Item -->
+        </div>
+        <div class="row justify-content-center mt-4">
+          <div class="col-lg-9">
+            <form action="contacts.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div><!-- End Contact Form -->
+        </div>
+      </div>
+    </section><!-- End Contact Section -->
   </main>
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
   <!-- Vendor JS Files -->
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
-
 </body>
 
 </html>
